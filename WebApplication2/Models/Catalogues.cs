5b87eb7b8e610caea924d15494914c592dd9aa5f
Esther <eckisa@icloud.com>
2025-01-14 14:28:25 +0100

    commit1

---
+++ WebApplication2/Models/Catalogues.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Catalogues
+    {
+        public int Id { get; set; }
+        public string nom { get; set; }
+        public decimal tva { get; set; }
+        public bool status { get; set; }
+    }
+}
---
+++ WebApplication2/Models/Exemple.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Exemple
+    {
+        public int Id { get; set; }
+        public string nom { get; set; }
+        public decimal tva { get; set; }
+        public bool status { get; set; }
+
+        //dj.backir@gmail.com
+    }
+}
